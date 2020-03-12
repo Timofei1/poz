9709b9d24d16be64510ba1778a4aa59fcc5b78ab
student <student@obr18.ru>
2020-03-12 09:43:10 +0400

    Updates

---
+++ precache-manifest.2bdebf15fc16e0a1b0bb1c8b8a1f6ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7902d3c21866f322506559678a0e5c59",
+    "url": "/poz/index.html"
+  },
+  {
+    "revision": "dc42d1dfd0dda5617242",
+    "url": "/poz/static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "68e2296cfcdaef0e2207",
+    "url": "/poz/static/css/main.64d374ef.chunk.css"
+  },
+  {
+    "revision": "dc42d1dfd0dda5617242",
+    "url": "/poz/static/js/2.8af6a706.chunk.js"
+  },
+  {
+    "revision": "68e2296cfcdaef0e2207",
+    "url": "/poz/static/js/main.bf0ab60c.chunk.js"
+  },
+  {
+    "revision": "8e1dc11ea52bacef493b",
+    "url": "/poz/static/js/runtime-main.83259f48.js"
+  }
+]);
